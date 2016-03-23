ad0eede01d619ca7509cbc3efd7d9b2307cbf8ce
Wilfred Hughes <me@wilfred.me.uk>
2016-03-23 08:27:22 +0000

    Loop with readline.

--- x64/step0_repl.s
+++ x64/step0_repl.s
@@ -2,8 +2,27 @@
 
         .text
 main:
+readloop:
+	## call readline(message);
         mov     $message, %rdi
+        call    readline
+
+        ## on EOF (e.g. pressing Ctrl-D), readline returns NULL.
+        ## Terminate when that happens.
+        cmpq    $0, %rax
+        je      end
+        
+        ## We need to clean up the string allocated by readline.
+        mov     %rax, %rdi
+        call    free
+
+        jmp     readloop
+
+end:
+        mov     $emptystr, %rdi
         call    puts
         ret
 message:
         .asciz "user> "            # asciz puts a 0 byte at the end
+emptystr:
+        .asciz ""
