930c4fce98018b670ef48236b536761cc713c047
Wilfred Hughes <me@wilfred.me.uk>
2016-03-24 22:59:11 +0000

    Unused file.

--- x64/hello.s
+++
@@ -1,9 +0,0 @@
-        .global main
-
-        .text
-main:
-        mov     $message, %rdi
-        call    puts
-        ret
-message:
-        .asciz "user> "            # asciz puts a 0 byte at the end
